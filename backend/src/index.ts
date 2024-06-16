4de29eab2c239bd208d8cc4345ffca96b8d306ed
sufiyanpathan <48600434+sufiyanpathan@users.noreply.github.com>
2024-06-16 15:23:36 +0530

    revert changes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,9 +7,9 @@ import authRouter from "./routes/auth";
 import hotelRouter from "./routes/my-hotels";
 import cookieParser from "cookie-parser";
 import path from "path";
-import { v2 } from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 
-v2.config({
+cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
